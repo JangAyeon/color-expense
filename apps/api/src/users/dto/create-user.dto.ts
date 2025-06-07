3c9c2e18c91678103c94a5ea572d0ef5424f4763
JangAyeon <ayeon3052@ewhain.net>
2025-06-07 15:13:43 +0900

    feat(api): use user id from supabase auth

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -2,6 +2,8 @@ import { IsNonEmptyString } from 'src/validate-decorators';
 
 export class CreateUserDto {
   @IsNonEmptyString()
+  name: string;
+  @IsNonEmptyString()
   email: string;
   @IsNonEmptyString()
   phone: string;
