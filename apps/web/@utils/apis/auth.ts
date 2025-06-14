16164d730e72c677c9d0aae8fbf18f2f5886ce29
JangAyeon <ayeon3052@ewhain.net>
2025-06-14 21:50:44 +0900

    feat(auth): add API routes for sign in and sign up functionality

---
+++ apps/web/@utils/apis/auth.ts
@@ -0,0 +1,46 @@
+import {
+  AuthUser,
+  SupabaseSignInResponse,
+  SupabaseSignUpResponse,
+} from "@repo/types";
+import { ApiRoute, AuthRoute } from "../../@constant/api.route";
+
+interface SupabaseSignInRequest {
+  email: AuthUser["email"];
+  password: string;
+}
+
+interface SupabaseSignUpRequest {
+  email: AuthUser["email"];
+  password: string;
+}
+
+export const fetchSignIn = async (
+  data: SupabaseSignInRequest
+): Promise<SupabaseSignInResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}${ApiRoute.AUTH}${AuthRoute.SIGN_IN}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
+  if (!res.ok) throw new Error("로그인 실패");
+  return res.json();
+};
+
+export const fetchSignUp = async (
+  data: SupabaseSignUpRequest
+): Promise<SupabaseSignUpResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}${ApiRoute.AUTH}${AuthRoute.SIGN_UP}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
+  if (!res.ok) throw new Error("회원가입 실패");
+  return res.json();
+};
