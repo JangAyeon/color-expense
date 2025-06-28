087645ee286f18e4cd94ff474333757beb294d80
JangAyeon <ayeon3052@ewhain.net>
2025-06-28 17:04:33 +0900

    feat(expense): add ExpenseLayout and ExpensePage components for expense management

---
+++ apps/web/app/expense/layout.tsx
@@ -0,0 +1,35 @@
+// app/budget/layout.tsx
+import { cookies } from "next/headers";
+import HydrationProvider from "../../@provider/hydration";
+import { fetchExpenseStatus } from "../../@utils/apis/expense";
+import { toYMDWithString } from "../../@utils/date/YMD";
+export default async function ExpenseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const access_token = cookieStore.get("access_token")?.value ?? null;
+
+  const today = new Date();
+  // const year = today.getFullYear().toString();
+  // const month = (today.getMonth() + 1).toString().padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
+  // const day = today.getDate().toString().padStart(2, "0"); // 일을 2자리로 맞추기 위해 padStart 사용
+  const { year, month, day } = toYMDWithString(today);
+  if (!access_token) {
+    return <div>No access token</div>;
+  }
+
+  return (
+    // <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>
+
+    <HydrationProvider
+      queryKey={["expense-monthly", year, month, day]}
+      queryFn={() =>
+        fetchExpenseStatus(year, month, day, "monthly", access_token)
+      }
+    >
+      {children}
+    </HydrationProvider>
+  );
+}
---
+++ apps/web/app/expense/page.tsx
@@ -0,0 +1,13 @@
+import ExpenseManager from "../../@component/expense/expenseManager";
+
+const ExpensePage = () => {
+  return (
+    <div>
+      <h1>Expense Page</h1>
+      <p>This is the expense page content.</p>
+      <ExpenseManager />
+    </div>
+  );
+};
+
+export default ExpensePage;
