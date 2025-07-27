fb36be39428a37348189f74be4dddf21f5c8bad6
JangAyeon <ayeon3052@ewhain.net>
2025-07-27 17:19:48 +0900

    feat: 연속 지출 기록 layer

--- apps/web/@hook/business/cube/useCube.ts
+++ apps/web/@hook/business/cube/useCube.ts
@@ -2,6 +2,7 @@ import { useBudgetStatus } from "@hook/api/budget/useBudget";
 import {
   useMonthlyExpenses,
   useExpensesCategory,
+  useExpensesStreak,
 } from "@hook/api/expense/useExpense";
 
 export const useCube = ({
@@ -21,33 +22,38 @@ export const useCube = ({
     month,
     day,
   });
-
+  const streakQuery = useExpensesStreak();
   const isLoading =
     budgetQuery.isLoading ||
     expenseCategoryQuery.isLoading ||
-    expensesQuery.isLoading;
+    expensesQuery.isLoading ||
+    streakQuery.isLoading;
   const hasError =
     budgetQuery.isError ||
     expenseCategoryQuery.isError ||
-    expensesQuery.isError;
+    expensesQuery.isError ||
+    streakQuery.isError;
   // budgetHistoryQuery.isError ||
   // recentExpensesQuery.isError;
   const isSuccess =
     budgetQuery.isSuccess ||
     expenseCategoryQuery.isSuccess ||
-    expensesQuery.isSuccess;
+    expensesQuery.isSuccess ||
+    streakQuery.isSuccess;
   // expensesQuery.isSuccess ||
   // recentExpensesQuery.isSuccess;
   console.log({
     budgetQuery: budgetQuery.data,
     expenseCategory: expenseCategoryQuery.data,
     expensesQuery: expensesQuery.data,
+    streakQuery: streakQuery.data,
   });
   return {
     // 개별 쿼리 상태
     budgetQuery: budgetQuery,
     expenseCategoryQuery: expenseCategoryQuery,
     expensesQuery,
+    streakQuery,
 
     // 통합 상태
     isLoading,
@@ -59,6 +65,7 @@ export const useCube = ({
       profile: budgetQuery.error,
       expenseCategory: expenseCategoryQuery.error,
       expensesQuery: expensesQuery.error,
+      streakQuery: streakQuery.error,
     },
   };
 };
